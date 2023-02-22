0c8510ba601c943e12d66acd43bf7c2e51a35dbb
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2023-02-22 20:47:54 +1100

    first commit

---
+++ src/main/java/io/bms/bmswk/BlackmesaWarehouseKeeperApiApplication.java
@@ -0,0 +1,13 @@
+package io.bms.bmswk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackmesaWarehouseKeeperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackmesaWarehouseKeeperApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/bms/bmswk/BlackmesaWarehouseKeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bms.bmswk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackmesaWarehouseKeeperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
