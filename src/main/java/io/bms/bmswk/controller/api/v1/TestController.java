145b05c26b6a08afb14e1d44ee6be3fe947e0236
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2023-03-04 18:57:22 +1100

    added test controller

--- src/main/java/io/bms/bmswk/controller/api/v1/TestController.java
+++ src/main/java/io/bms/bmswk/controller/api/v1/TestController.java
@@ -5,6 +5,7 @@ import io.bms.bmswk.security.constant.SecurityConstant;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,24 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 996Worker
  * @since 2023-03-03 10:32
  */
-@RestController("/api/v1/test")
+@RestController
+@RequestMapping("/api/v1/test")
 public class TestController {
 
     @GetMapping("/purchase")
     @RequiresPermissions({SecurityConstant.INVENTORY_PURCHASE_PERMISSION})
     public R testPurchasePermission() {
-        return R.ok().setData((String) SecurityUtils.getSubject().getPrincipal());
+        return R.ok().setData(SecurityUtils.getSubject().getPrincipal());
     }
 
     @GetMapping("/consume")
     @RequiresPermissions({SecurityConstant.INVENTORY_CONSUME_PERMISSION})
     public R testConsumePermission() {
-        return R.ok().setData((String) SecurityUtils.getSubject().getPrincipal());
+        return R.ok().setData(SecurityUtils.getSubject().getPrincipal());
     }
 
     @GetMapping("/user_manage")
     @RequiresPermissions({SecurityConstant.USER_MANAGE_PERMISSION})
     public R testUsermanagePermission() {
-        return R.ok().setData((String) SecurityUtils.getSubject().getPrincipal());
+        return R.ok().setData(SecurityUtils.getSubject().getPrincipal());
     }
 }
