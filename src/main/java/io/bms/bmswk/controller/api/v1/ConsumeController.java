11d6aaf1f09300fc883103ea43fbfbf3c1aa94cb
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2023-03-06 17:12:27 +1100

    update consume and purchase VO

--- src/main/java/io/bms/bmswk/controller/api/v1/ConsumeController.java
+++ src/main/java/io/bms/bmswk/controller/api/v1/ConsumeController.java
@@ -3,10 +3,18 @@ package io.bms.bmswk.controller.api.v1;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.bms.bmswk.model.entity.Consume;
+import io.bms.bmswk.model.entity.Sku;
+import io.bms.bmswk.model.entity.User;
+import io.bms.bmswk.model.entity.Warehouse;
 import io.bms.bmswk.model.param.ConsumeCreateParam;
 import io.bms.bmswk.model.support.R;
+import io.bms.bmswk.model.vo.ConsumeVO;
 import io.bms.bmswk.security.constant.SecurityConstant;
 import io.bms.bmswk.service.IConsumeService;
+import io.bms.bmswk.service.ISkuService;
+import io.bms.bmswk.service.IUserService;
+import io.bms.bmswk.service.IWarehouseService;
+import io.bms.bmswk.util.BeanUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -15,6 +23,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * <p>
@@ -32,13 +42,38 @@ public class ConsumeController {
     @Autowired
     private IConsumeService consumeService;
 
+    @Autowired
+    private ISkuService skuService;
+
+    @Autowired
+    private IWarehouseService warehouseService;
+
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/{consumeId}")
     @RequiresPermissions(
             value = {SecurityConstant.INVENTORY_CONSUME_PERMISSION, SecurityConstant.INVENTORY_MANAGE_PERMISSION},
             logical = Logical.OR
     )
-    public R getConsumeById(@PathVariable String consumeId) {
-        return R.ok().setData(consumeService.getById(Integer.parseInt(consumeId)));
+    public R getConsumeById(@PathVariable Integer consumeId) {
+        Consume theConsume = consumeService.getById(consumeId);
+        ConsumeVO consumeVO = BeanUtils.transformFrom(theConsume, ConsumeVO.class);
+        Sku theSku = skuService.getById(theConsume.getSkuId());
+        Warehouse theWarehouse = warehouseService.getById(theConsume.getWarehouseId());
+        User consumer = userService.getById(theConsume.getConsumerId());
+        User keeper;
+
+        if (theConsume.getKeeperId() != null) {
+            keeper = userService.getById(theConsume.getKeeperId());
+            consumeVO.setKeeperName(keeper.getName());
+        }
+
+        consumeVO.setSkuName(theSku.getName());
+        consumeVO.setWarehouseName(theWarehouse.getName());
+        consumeVO.setConsumerName(consumer.getName());
+        consumeVO.setConsumeId(consumeId);
+        return R.ok().setData(consumeVO);
     }
 
     @GetMapping("/all")
@@ -55,7 +90,9 @@ public class ConsumeController {
             Page<Consume> thePage = consumeService.page(
                     new Page<>(page, size)
             );
-            return R.ok().setData(thePage.getRecords());
+
+            List<ConsumeVO> consumeVOList = genConsumeVOListByPage(thePage);
+            return R.ok().setData(consumeVOList);
         }
 
         QueryWrapper<Consume> wrapper = new QueryWrapper<>();
@@ -64,7 +101,8 @@ public class ConsumeController {
                 new Page<>(page, size), wrapper
         );
 
-        return R.ok().setData(thePage.getRecords());
+        List<ConsumeVO> consumeVOList = genConsumeVOListByPage(thePage);
+        return R.ok().setData(consumeVOList);
     }
 
     @PostMapping("")
@@ -100,4 +138,33 @@ public class ConsumeController {
         return R.ok();
     }
 
+    /**
+     * gen vos
+     * @param thePage page query result
+     * @return vos
+     */
+    private List<ConsumeVO> genConsumeVOListByPage(Page<Consume> thePage) {
+        List<ConsumeVO> consumeVOList = new LinkedList<>();
+
+        thePage.getRecords().forEach(theConsume -> {
+            ConsumeVO consumeVO = BeanUtils.transformFrom(theConsume, ConsumeVO.class);
+            Sku theSku = skuService.getById(theConsume.getSkuId());
+            Warehouse theWarehouse = warehouseService.getById(theConsume.getWarehouseId());
+            User consumer = userService.getById(theConsume.getConsumerId());
+            User keeper;
+
+            if (theConsume.getKeeperId() != null) {
+                keeper = userService.getById(theConsume.getKeeperId());
+                consumeVO.setKeeperName(keeper.getName());
+            }
+
+            consumeVO.setSkuName(theSku.getName());
+            consumeVO.setWarehouseName(theWarehouse.getName());
+            consumeVO.setConsumerName(consumer.getName());
+            consumeVO.setConsumeId(theConsume.getId());
+
+            consumeVOList.add(consumeVO);
+        });
+        return consumeVOList;
+    }
 }
--- src/main/java/io/bms/bmswk/controller/api/v1/PurchaseController.java
+++ src/main/java/io/bms/bmswk/controller/api/v1/PurchaseController.java
@@ -3,10 +3,18 @@ package io.bms.bmswk.controller.api.v1;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.bms.bmswk.model.entity.Purchase;
+import io.bms.bmswk.model.entity.Sku;
+import io.bms.bmswk.model.entity.User;
+import io.bms.bmswk.model.entity.Warehouse;
 import io.bms.bmswk.model.param.PurchaseCreateParam;
 import io.bms.bmswk.model.support.R;
+import io.bms.bmswk.model.vo.PurchaseVO;
 import io.bms.bmswk.security.constant.SecurityConstant;
 import io.bms.bmswk.service.IPurchaseService;
+import io.bms.bmswk.service.ISkuService;
+import io.bms.bmswk.service.IUserService;
+import io.bms.bmswk.service.IWarehouseService;
+import io.bms.bmswk.util.BeanUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -15,6 +23,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * <p>
@@ -32,12 +42,37 @@ public class PurchaseController {
     @Autowired
     private IPurchaseService purchaseService;
 
+    @Autowired
+    private ISkuService skuService;
+
+    @Autowired
+    private IWarehouseService warehouseService;
+
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/{purchaseId}")
     @RequiresPermissions(
             value = {SecurityConstant.INVENTORY_PURCHASE_PERMISSION, SecurityConstant.INVENTORY_MANAGE_PERMISSION},
             logical = Logical.OR)
-    public R getPurchaseById(@PathVariable String purchaseId) {
-        return R.ok().setData(purchaseService.getById(Integer.parseInt(purchaseId)));
+    public R getPurchaseById(@PathVariable Integer purchaseId) {
+        Purchase thePurchase = purchaseService.getById(purchaseId);
+        PurchaseVO purchaseVO = BeanUtils.transformFrom(thePurchase, PurchaseVO.class);
+        Sku theSku = skuService.getById(thePurchase.getSkuId());
+        Warehouse theWarehouse = warehouseService.getById(thePurchase.getWarehouseId());
+        User purchaser = userService.getById(thePurchase.getPurchaserId());
+        User keeper;
+        if (thePurchase.getKeeperId() != null) {
+            keeper = userService.getById(thePurchase.getKeeperId());
+            purchaseVO.setKeeperName(keeper.getName());
+        }
+
+        purchaseVO.setSkuName(theSku.getName());
+        purchaseVO.setWarehouseName(theWarehouse.getName());
+        purchaseVO.setPurchaserName(purchaser.getName());
+        purchaseVO.setPurchaseId(purchaseId);
+
+        return R.ok().setData(purchaseVO);
     }
 
     @GetMapping("/all")
@@ -52,14 +87,16 @@ public class PurchaseController {
 
         if (status == null) {
             Page<Purchase> thePage = purchaseService.page(new Page<>(page, size));
-            return R.ok().setData(thePage.getRecords());
+            List<PurchaseVO> purchaseVOList = genPurchaseVOListByPageQuery(thePage);
+            return R.ok().setData(purchaseVOList);
         } else {
             QueryWrapper<Purchase> wrapper = new QueryWrapper<>();
             wrapper.eq("status", status);
             Page<Purchase> thePage = purchaseService.page(
                     new Page<>(page, size), wrapper
             );
-            return R.ok().setData(thePage.getRecords());
+            List<PurchaseVO> purchaseVOList = genPurchaseVOListByPageQuery(thePage);
+            return R.ok().setData(purchaseVOList);
         }
     }
 
@@ -95,4 +132,31 @@ public class PurchaseController {
         return R.ok();
     }
 
+    /**
+     * generate vo list
+     * @param thePage page query result
+     * @return list of vos
+     */
+    private List<PurchaseVO> genPurchaseVOListByPageQuery(Page<Purchase> thePage) {
+        List<PurchaseVO> purchaseVOList = new LinkedList<>();
+        thePage.getRecords().forEach(thePurchase -> {
+            PurchaseVO purchaseVO = BeanUtils.transformFrom(thePurchase, PurchaseVO.class);
+            Sku theSku = skuService.getById(thePurchase.getSkuId());
+            Warehouse theWarehouse = warehouseService.getById(thePurchase.getWarehouseId());
+            User purchaser = userService.getById(thePurchase.getPurchaserId());
+            User keeper;
+            if (thePurchase.getKeeperId() != null) {
+                keeper = userService.getById(thePurchase.getKeeperId());
+                purchaseVO.setKeeperName(keeper.getName());
+            }
+
+            purchaseVO.setSkuName(theSku.getName());
+            purchaseVO.setWarehouseName(theWarehouse.getName());
+            purchaseVO.setPurchaserName(purchaser.getName());
+            purchaseVO.setPurchaseId(thePurchase.getId());
+
+            purchaseVOList.add(purchaseVO);
+        });
+        return purchaseVOList;
+    }
 }
